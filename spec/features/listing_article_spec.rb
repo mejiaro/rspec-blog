a3e41b3bd937551b9abedfe670a147d75afeef73
Ricardo Mejía <mejiaro@gmail.com>
2017-10-27 15:59:34 -0600

    last listing articles changes

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -19,4 +19,23 @@ RSpec.feature "Listing articles" do
 		expect(page).to have_link(@article0.title)
 		expect(page).to have_link(@article1.title)
 	end
+
+	scenario "No articles" do
+		Article.delete_all
+
+		visit "/"
+
+
+		expect(page).not_to have_content(@article0.title)
+		expect(page).not_to have_content(@article1.title)
+
+		expect(page).not_to have_link(@article0.title)
+		expect(page).not_to have_link(@article1.title)
+
+		within("h1#no-articles") do 
+			expect(page).to have_content("No articles created")
+		end
+
+
+	end
 end
